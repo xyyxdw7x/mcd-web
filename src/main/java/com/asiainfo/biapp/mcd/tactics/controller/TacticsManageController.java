aec7ac1860358c217c45a364d58e21deab63a37e
wangbin9 <wangbin9@10.1.48.97>
2016-08-18 18:38:19 +0800

    a

--- src/main/java/com/asiainfo/biapp/mcd/tactics/controller/TacticsManageController.java
+++ src/main/java/com/asiainfo/biapp/mcd/tactics/controller/TacticsManageController.java
@@ -1,9 +1,16 @@
 package com.asiainfo.biapp.mcd.tactics.controller;
 
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.asiainfo.biapp.framework.web.controller.BaseMultiActionController;
+import com.asiainfo.biframe.utils.string.StringUtil;
+
+import net.sf.json.JSONObject;
 
 public class TacticsManageController extends BaseMultiActionController{
 	
@@ -15,8 +22,6 @@ public class TacticsManageController extends BaseMultiActionController{
 	 * @param response
 	 * @throws Exception
 	 */
-	public void saveCampsegWaveInfo(HttpServletRequest request,HttpServletResponse response)  throws Exception {
-		
-	}
+	public void saveCampsegWaveInfo(HttpServletRequest request,HttpServletResponse response)  throws Exception {}
 
 }
