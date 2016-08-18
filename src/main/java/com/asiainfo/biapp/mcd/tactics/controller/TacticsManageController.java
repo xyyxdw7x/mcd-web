cb36667798c11642f1d06c969f0656e044a1f7a8
wangbin9 <wangbin9@asiainfo.com>
2016-08-18 19:21:40 +0800

    a

---
+++ src/main/java/com/asiainfo/biapp/mcd/tactics/controller/TacticsManageController.java
@@ -0,0 +1,7 @@
+package com.asiainfo.biapp.mcd.tactics.controller;
+
+import com.asiainfo.biapp.framework.web.controller.BaseMultiActionController;
+
+public class TacticsManageController extends BaseMultiActionController{
+
+}
