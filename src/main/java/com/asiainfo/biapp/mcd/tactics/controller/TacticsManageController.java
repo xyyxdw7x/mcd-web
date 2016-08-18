3593e802ca43ce2f9b8b52eb6674e6eb9a3caf0f
wangbin9 <wangbin9@10.1.48.97>
2016-08-18 17:59:02 +0800

    层次

---
+++ src/main/java/com/asiainfo/biapp/mcd/tactics/controller/TacticsManageController.java
@@ -0,0 +1,22 @@
+package com.asiainfo.biapp.mcd.tactics.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.asiainfo.biapp.framework.web.controller.BaseMultiActionController;
+
+public class TacticsManageController extends BaseMultiActionController{
+	
+	/**
+	 * 保存策略基本信息
+	 * @param mapping
+	 * @param form
+	 * @param request
+	 * @param response
+	 * @throws Exception
+	 */
+	public void saveCampsegWaveInfo(HttpServletRequest request,HttpServletResponse response)  throws Exception {
+		
+	}
+
+}
