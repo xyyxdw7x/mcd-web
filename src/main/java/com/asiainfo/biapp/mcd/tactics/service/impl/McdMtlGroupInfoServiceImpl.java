23d2b824ae7b262faf1129ca8c3d5775deb13bf6
hanjn <hanjn@asiainfo.com>
2016-08-25 16:02:55 +0800

    add

---
+++ src/main/java/com/asiainfo/biapp/mcd/tactics/service/impl/McdMtlGroupInfoServiceImpl.java
@@ -0,0 +1,23 @@
+package com.asiainfo.biapp.mcd.tactics.service.impl;
+
+import javax.annotation.Resource;
+
+import org.springframework.stereotype.Repository;
+
+import com.asiainfo.biapp.mcd.tactics.dao.IMcdMtlGroupInfoDao;
+import com.asiainfo.biapp.mcd.tactics.service.IMcdMtlGroupInfoService;
+import com.asiainfo.biapp.mcd.tactics.vo.MtlGroupInfo;
+
+@Repository("mcdMtlGroupInfoService")
+public class McdMtlGroupInfoServiceImpl implements IMcdMtlGroupInfoService{
+    @Resource(name="mcdMtlGroupInfoDao")
+    private IMcdMtlGroupInfoDao mcdMtlGroupInfoDao;
+    /**
+     * 根据客户群ID查找客户群信息
+     */
+    @Override
+    public MtlGroupInfo getMtlGroupInfo(String custgroupId) {
+        return mcdMtlGroupInfoDao.getMtlGroupInfo(custgroupId);
+    }
+
+}
