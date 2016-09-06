50a14a769de8250e5e09d4be920d3a9a8fe78fa4
wangbin9 <wangbin9@asiainfo.com>
2016-09-06 15:19:11 +0800

    优化

--- src/main/java/com/asiainfo/biapp/mcd/tactics/controller/TacticsManageController.java
+++ src/main/java/com/asiainfo/biapp/mcd/tactics/controller/TacticsManageController.java
@@ -6,11 +6,9 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
-import java.util.Set;
 import java.util.regex.Pattern;
 
 import javax.annotation.Resource;
