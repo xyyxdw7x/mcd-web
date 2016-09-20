bafc9b18944ec1452c1ea06661775da1999f514b
hanjn <hanjn@asiainfo.com>
2016-09-20 16:04:41 +0800

    删除不用的类

--- src/main/java/com/asiainfo/biapp/mcd/common/util/MpmConfigure.java
+++ src/main/java/com/asiainfo/biapp/mcd/common/util/MpmConfigure.java
@@ -13,7 +13,6 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.apache.commons.lang3.StringUtils;
 
-import com.asiainfo.biapp.framework.core.AppConfigService;
 
 public class MpmConfigure {
 
