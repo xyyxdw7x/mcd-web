206b6f2a13bc5ab01bdce73641cd094fd027269e
hanjn <hanjn@asiainfo.com>
2016-09-20 16:48:50 +0800

    测试分支冲突

--- src/main/java/com/asiainfo/biapp/mcd/test/controller/HttlController.java
+++ src/main/java/com/asiainfo/biapp/mcd/test/controller/HttlController.java
@@ -28,7 +28,7 @@ public class HttlController extends BaseMultiActionController {
 	@RequestMapping
 	public ModelAndView findBooks(HttpServletRequest request,
 			HttpServletResponse response) throws Exception {
-		String name=request.getParameter("name");
+		String name=request.getParameter("name2");
 		String name2=request.getParameter("name");
 		ModelAndView model = new ModelAndView("test/books");
 		HashMap<String,Object> modelMap=new HashMap<String,Object>();
