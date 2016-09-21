2dba6e09180264bfabb957855c99e63e709e190f
hanjn <hanjn@asiainfo.com>
2016-09-21 17:13:51 +0800

    添加字典数据到缓存中

--- src/main/java/com/asiainfo/biapp/mcd/common/service/impl/ApplicationCompleteSysTask.java
+++ src/main/java/com/asiainfo/biapp/mcd/common/service/impl/ApplicationCompleteSysTask.java
@@ -8,6 +8,7 @@ import java.util.concurrent.ConcurrentMap;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.springframework.stereotype.Service;
 
 import com.asiainfo.biapp.framework.core.AppConfigService;
 import com.asiainfo.biapp.framework.core.context.IApplicationContextRefreshed;
@@ -19,6 +20,7 @@ import com.asiainfo.biapp.mcd.common.dao.ISysDicDao;
  * @author hjn
  *
  */
+@Service
 public class ApplicationCompleteSysTask implements IApplicationContextRefreshed {
 
 	protected final Log log = LogFactory.getLog(getClass());
@@ -54,22 +56,23 @@ public class ApplicationCompleteSysTask implements IApplicationContextRefreshed 
 		for(int i=0,size=sourceList.size();i<size;i++){
 			Map<String,Object> item=sourceList.get(i);
 			String key=(String) item.get("DIC_KEY");
+			String value=(String)item.get("DIC_DATA_VALUE");
+			List<String> valueList=new ArrayList<String>();
 			//key-value结构直接保存
 			if(item.get("DIC_DATA_PVALUE")==null&&item.get("DIC_DATA_VALUE")!=null){
-				cMap.put(key, item);
+				cMap.put(key, value);
 			}else if(item.get("DIC_DATA_PVALUE")==null&&item.get("DIC_DATA_VALUE")==null){
 				//只保存一个key
-				cMap.put(key, item);
+				cMap.put(key,valueList);
 			}else if(item.get("DIC_DATA_PVALUE")!=null&&item.get("DIC_DATA_VALUE")!=null){
 				// 找到key 再存value到List中
-				List<Map<String,Object>> valueList=null;
 				if(cMap.containsKey(key)){
-					valueList=(List<Map<String, Object>>) cMap.get(key);
+					valueList=(List<String>) cMap.get(key);
 				}else{
-					valueList=new ArrayList<Map<String,Object>>();
+					valueList=new ArrayList<String>();
 					cMap.put(key, valueList);
 				}
-				valueList.add(item);
+				valueList.add(value);
 			}
 		}
 		return cMap;
