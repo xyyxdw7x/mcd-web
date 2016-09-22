9b20320bb984bcf9d683f7aab1fee2aefa2eb556
wangbin9 <wangbin9@asiainfo.com>
2016-09-22 20:40:34 +0800

    去掉警告

--- src/main/java/com/asiainfo/biapp/mcd/tactics/dao/impl/ChannelBossSmsTemplateDaoImpl.java
+++ src/main/java/com/asiainfo/biapp/mcd/tactics/dao/impl/ChannelBossSmsTemplateDaoImpl.java
@@ -2,7 +2,6 @@ package com.asiainfo.biapp.mcd.tactics.dao.impl;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
