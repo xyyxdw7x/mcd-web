29f9a819fe837cb991ca64640e782cf20ce30fc0
罗朝华 <luoch@asiainfo.com>
2016-09-22 17:43:41 +0800

    渠道展示修改

--- src/main/webapp/assets/js/tactics/provinces/jx/channel/collect/901.js
+++ src/main/webapp/assets/js/tactics/provinces/jx/channel/collect/901.js
@@ -2,10 +2,10 @@ function collectData(event,data){
 	var channelContentInfo = new Object();
 	channelContentInfo.channelId = data.channelId;
 	channelContentInfo.channelName = data.channelName;
-	channelContentInfo.channelContentWords = $("#channelId_"+data.channelId+"_contentWords").val();
+	channelContentInfo.execContent = $("#channelId_"+data.channelId+"_contentWords").val();
 	channelContentInfo.isLoopSend = $("#channelId_"+data.channelId+"_sendOnce").hasClass("active")? 1:0;
-	//channelContentInfo.isAssignSendDate = "";
-	//channelContentInfo.sendCycle = $("channelId_"+data.channelId+"_sendOnce").hasClass("active")? -1:0;
+	channelContentInfo.isHasVar = "1";
+	
 	
 	return channelContentInfo;
 }
--- src/main/webapp/assets/js/tactics/provinces/jx/channel/collect/910.js
+++ src/main/webapp/assets/js/tactics/provinces/jx/channel/collect/910.js
@@ -2,7 +2,8 @@ function collectData(event,data){
 	var channelContentInfo = new Object();
 	channelContentInfo.channelId = data.channelId;
 	channelContentInfo.channelName = data.channelName;
-	channelContentInfo.channelContentWords = $("#channelId_"+data.channelId+"_contentWords").attr("value");
+	channelContentInfo.execContent = $("#channelId_"+data.channelId+"_contentWords").val();
+	channelContentInfo.adivId="1335491647281";
 	
 	return channelContentInfo;
 }
