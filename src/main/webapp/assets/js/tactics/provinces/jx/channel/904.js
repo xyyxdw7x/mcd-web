6f4bc00099e97a5b487f5e568bded04450be24c2
罗朝华 <luoch@asiainfo.com>
2016-09-29 16:40:29 +0800

    904渠道修改

--- src/main/webapp/assets/js/tactics/provinces/jx/channel/904.js
+++ src/main/webapp/assets/js/tactics/provinces/jx/channel/904.js
@@ -2,41 +2,41 @@
  * 渠道选择封装类
  * baseInfo渠道基本信息，包含渠道ID 渠道名称
  */
-var channelInfo902={baseInfo:null};
+var channelInfo904={baseInfo:null};
 
 /**
  * 渠道初始化函数  在主体架构中会动态调用该函数
  */
-function initView902(data){
-	channelInfo902.baseInfo=data;
-	channelInfo902.initView();
+function initView904(data){
+	channelInfo904.baseInfo=data;
+	channelInfo904.initView();
 }
 /**
  * 界面上初始化、事件监听
  */
-channelInfo902.initView=function(){
+channelInfo904.initView=function(){
 	//加载一些界面数据
-	channelInfo902.loadSomeBaseData902();
+	channelInfo904.loadSomeBaseData904();
 	//添加事件监听
-	channelInfo902.clickChannelContentEventHandler902();
+	channelInfo904.clickChannelContentEventHandler904();
 }
 
 /**
  * 加载界面上渠道下的基础数据
  */
-channelInfo902.loadSomeBaseData902=function(){
+channelInfo904.loadSomeBaseData904=function(){
 	
 }
 
 /**
  * 界面上各个元素的事件处理
  */
-channelInfo902.clickChannelContentEventHandler902=function(){
+channelInfo904.clickChannelContentEventHandler904=function(){
 	//确定按钮
-	channelInfo902.clickCommitButtonEventHandler902();
+	channelInfo904.clickCommitButtonEventHandler904();
 	
 	//输入字数时对字数限制
-	channelInfo902.textAreaInputNumTip902();
+	channelInfo904.textAreaInputNumTip904();
 	
 }
 
@@ -44,17 +44,17 @@ channelInfo902.clickChannelContentEventHandler902=function(){
  * 收集此渠道的渠道表单内容
  * @returns 渠道表单内容
  */
-channelInfo902.collectData902=function(){
+channelInfo904.collectData904=function(){
 	var channelContentInfo = new Object();
 	channelContentInfo.keys = new Array();
 	channelContentInfo.values = new Array();
-	channelContentInfo.channelId = channelInfo902.baseInfo.channelId;
-	channelContentInfo.channelName = channelInfo902.baseInfo.channelName;
-	channelContentInfo.execContent = $("#channelId_"+channelInfo902.baseInfo.channelId+"_contentWords").val();
+	channelContentInfo.channelId = channelInfo904.baseInfo.channelId;
+	channelContentInfo.channelName = channelInfo904.baseInfo.channelName;
+	channelContentInfo.execContent = $("#channelId_"+channelInfo904.baseInfo.channelId+"_contentWords").val();
 	
 	channelContentInfo.keys[0] = "渠道名称";
 	channelContentInfo.keys[1] = "推荐营销用语";
-	channelContentInfo.values[0] = channelInfo902.baseInfo.channelName;
+	channelContentInfo.values[0] = channelInfo904.baseInfo.channelName;
 	channelContentInfo.values[1] = channelContentInfo.execContent;
 	
 	return channelContentInfo;
@@ -63,16 +63,16 @@ channelInfo902.collectData902=function(){
 /**
  * 录入渠道的相关信息后，点击确定按钮操作的处理。</br>
  */
-channelInfo902.clickCommitButtonEventHandler902=function(){
-	$("#commitButton_channelId_"+channelInfo902.baseInfo.channelId).click(function(){
+channelInfo904.clickCommitButtonEventHandler904=function(){
+	$("#commitButton_channelId_"+channelInfo904.baseInfo.channelId).click(function(){
 		//输入项校验
-		var checkResult = channelInfo902.checkValidation();
+		var checkResult = channelInfo904.checkValidation();
 		if(!checkResult[0]){
 			alert(checkResult[1]);
 			return ;
 		}
 		
-		var newdata = channelInfo902.collectData902();
+		var newdata = channelInfo904.collectData904();
 		
 		$("#channelDiv").trigger("changeChannel", newdata);
 	});
@@ -81,21 +81,21 @@ channelInfo902.clickCommitButtonEventHandler902=function(){
 /**
  * 营销用语输入字数限制
  */
-channelInfo902.textAreaInputNumTip902=function(){
+channelInfo904.textAreaInputNumTip904=function(){
 	//输入字数时对字数限制
-	var $textArea=$("#channelId_"+channelInfo902.baseInfo.channelId+"_contentWords");
-	var $maxNum=$("#channelId_"+channelInfo902.baseInfo.channelId+"_wordSize");
+	var $textArea=$("#channelId_"+channelInfo904.baseInfo.channelId+"_contentWords");
+	var $maxNum=$("#channelId_"+channelInfo904.baseInfo.channelId+"_wordSize");
 	textAreaInputNumTip($textArea,$maxNum);
 }
 
 /**
  * 确定、预览前的合法性检查
  */
-channelInfo902.checkValidation=function(){
+channelInfo904.checkValidation=function(){
 	var result = new Array();
 	result[0]=true;
 	
-	var contentWords = $("#channelId_"+channelInfo902.baseInfo.channelId+"_contentWords").val();
+	var contentWords = $("#channelId_"+channelInfo904.baseInfo.channelId+"_contentWords").val();
 	if(contentWords.length<1){
 		result[0]=false;
 		result[1]="必须录入推荐营销用语!";
