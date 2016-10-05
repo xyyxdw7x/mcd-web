518b2048a27d1c7baeb135ab05b3c539952af520
hanjn <hanjn@asiainfo.com>
2016-10-05 11:25:14 +0800

    修改js设计

--- src/main/webapp/assets/js/core.js
+++ src/main/webapp/assets/js/core.js
@@ -112,3 +112,50 @@ function loadJsFile(url,callBack,data){
 	document.body.appendChild(jsFile);
 }
 
+/***
+ *   通用引入js 文件 
+ * 
+ */
+
+seajs.config({
+	base :_ctx + "/mcd/assets/scripts/",
+	alias : {
+		"underscore" : "backbone/underscore-min.js",
+		"backbone" : "backbone/backbone-min.js",
+		"ejs" : "EJS/ejs_production.js",
+		"less" : "less/less-1.7.1.min.js",
+		"jquery": "jquery/jquery-1.10.2.js",
+		"jqueryExtend": "jquery/jquery.extend.js",
+		"jqueryUI": "jquery/jqueryUI/jquery-ui-1.11.0.min.js" ,
+		"onepageScroll": "jquery/jqueryUI/jquery.onepage-scroll.min.js" ,
+		"modernizr": "jquery/jqueryUI/modernizr.custom.07427.js" ,
+		"tacticsManage":"tactics/tacticsManage.js",
+		"My97DatePicker":"My97DatePicker/WdatePicker.js",
+		"navManage":"nav/navManage.js",
+		"parabola":"parabola.js",
+		"ajaxFileUpload":"jquery/ajaxfileupload.js",
+		"unslider":"unslider.min.js",
+		"bootstrap":"bootstrap/js/bootstrap.min.js",
+		"realTime":"tactics/realTime.js",
+//		"baiduMap":"http://api.map.baidu.com/api?v=2.0&ak=pOvQ10UjwGlvdThzxSYnjGEo",
+//		"baiduMapExtend":"http://api.map.baidu.com/library/DrawingManager/1.4/src/DrawingManager_min.js",
+
+		"echarts":"echarts/echarts-all.js",
+		"policyEffectPage":"effectAppraisal/policyEffectPage.js",
+		"policyViewDetail":"effectAppraisal/policyViewDetailEffectPage.js",
+		"LoadPolicyViewDetails":"effectAppraisal/LoadPolicyViewDetails.js",
+		"frontLineEffectPage":"effectAppraisal/frontLineEffectPage.js",
+		"frontLineDetailEffectPage":"effectAppraisal/frontLineDetailEffectPage.js",
+		"channelEffectPage":"effectAppraisal/channelEffectPage.js",
+		"effectOverview":"effectAppraisal/effectOverview.js",
+		"LoadOverview":"effectAppraisal/LoadOverview.js",
+		"MSMChartMain":"echartsUtil/MSMChartMain.js",
+		"MSMChart":"echartsUtil/MSMChart.js",
+		"DateUtil":"echartsUtil/DateUtil.js",
+		
+		
+		"effectMain":"effectAppraisal/effectMain.js",
+	} ,
+	preload : [ "jquery", "ejs", "less" ,"underscore"/*,"bootstrap"*/]
+}); 
+
