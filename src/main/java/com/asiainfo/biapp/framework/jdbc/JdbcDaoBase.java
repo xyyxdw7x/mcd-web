e28aef89807621ed5d094b0bc2e26e6f2af739cb
hanjn <hanjn@asiainfo.com>
2016-10-12 16:16:39 +0800

    修改数据源切换问题

--- src/main/java/com/asiainfo/biapp/framework/jdbc/JdbcDaoBase.java
+++ src/main/java/com/asiainfo/biapp/framework/jdbc/JdbcDaoBase.java
@@ -13,10 +13,12 @@ import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.dao.DataAccessException;
-import org.springframework.jdbc.core.support.JdbcDaoSupport;
+import org.springframework.dao.support.DaoSupport;
+import org.springframework.jdbc.datasource.DataSourceUtils;
+import org.springframework.jdbc.support.SQLExceptionTranslator;
 import org.springframework.orm.ObjectRetrievalFailureException;
-
-
+import org.springframework.jdbc.CannotGetJdbcConnectionException;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.ResultSetExtractor;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 
@@ -26,7 +28,7 @@ import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
  * RowMapper通常是一个静态的内部类
  *
  */
-public class JdbcDaoBase extends JdbcDaoSupport implements InitializingBean,DisposableBean{
+public class JdbcDaoBase extends DaoSupport implements InitializingBean,DisposableBean{
 
 	
 	@Autowired
@@ -111,6 +113,10 @@ public class JdbcDaoBase extends JdbcDaoSupport implements InitializingBean,Disp
 	 */
 	protected void initParameterJdbcTemplateConfig() {
 	}
+	
+
+	
+	
 	/**
 	 * 设置数据库的类型 
 	 * @param dataSource
@@ -150,11 +156,6 @@ public class JdbcDaoBase extends JdbcDaoSupport implements InitializingBean,Disp
 		super.initDao();
 	}
 
-	@Override
-	protected void checkDaoConfig() {
-		initialize();
-		super.checkDaoConfig();
-	}
 
 	@Override
 	public void destroy() throws Exception {
@@ -162,11 +163,111 @@ public class JdbcDaoBase extends JdbcDaoSupport implements InitializingBean,Disp
 	}
 
 	public JdbcTemplateTool getJdbcTemplateTool() {
+		
 		return jdbcTemplateTool;
 	}
 
 	public void setJdbcTemplateTool(JdbcTemplateTool jdbcTemplateTool) {
 		this.jdbcTemplateTool = jdbcTemplateTool;
 	}
+	
+	private JdbcTemplate jdbcTemplate;
+
+
+	/**
+	 * Set the JDBC DataSource to be used by this DAO.
+	 */
+	public final void setDataSource(DataSource dataSource) {
+		if (this.jdbcTemplate == null || dataSource != this.jdbcTemplate.getDataSource()) {
+			this.jdbcTemplate = createJdbcTemplate(dataSource);
+			initTemplateConfig();
+		}
+	}
+
+	/**
+	 * Create a JdbcTemplate for the given DataSource.
+	 * Only invoked if populating the DAO with a DataSource reference!
+	 * <p>Can be overridden in subclasses to provide a JdbcTemplate instance
+	 * with different configuration, or a custom JdbcTemplate subclass.
+	 * @param dataSource the JDBC DataSource to create a JdbcTemplate for
+	 * @return the new JdbcTemplate instance
+	 * @see #setDataSource
+	 */
+	protected JdbcTemplate createJdbcTemplate(DataSource dataSource) {
+		return new JdbcTemplate(dataSource);
+	}
+
+
+	/**
+	 * Set the JdbcTemplate for this DAO explicitly,
+	 * as an alternative to specifying a DataSource.
+	 */
+	public final void setJdbcTemplate(JdbcTemplate jdbcTemplate) {
+		this.jdbcTemplate = jdbcTemplate;
+		initTemplateConfig();
+	}
+
+	/**
+	 * Return the JdbcTemplate for this DAO,
+	 * pre-initialized with the DataSource or set explicitly.
+	 */
+	public  JdbcTemplate getJdbcTemplate() {
+		this.jdbcTemplate.setDataSource(this.getDataSource());;
+	  return this.jdbcTemplate;
+	}
+
+	/**
+	 * Initialize the template-based configuration of this DAO.
+	 * Called after a new JdbcTemplate has been set, either directly
+	 * or through a DataSource.
+	 * <p>This implementation is empty. Subclasses may override this
+	 * to configure further objects based on the JdbcTemplate.
+	 * @see #getJdbcTemplate()
+	 */
+	protected void initTemplateConfig() {
+	}
+
+	@Override
+	protected void checkDaoConfig() {
+		initialize();
+		if (this.jdbcTemplate == null) {
+			throw new IllegalArgumentException("'dataSource' or 'jdbcTemplate' is required");
+		}
+	}
+
+
+	/**
+	 * Return the SQLExceptionTranslator of this DAO's JdbcTemplate,
+	 * for translating SQLExceptions in custom JDBC access code.
+	 * @see org.springframework.jdbc.core.JdbcTemplate#getExceptionTranslator()
+	 */
+	protected final SQLExceptionTranslator getExceptionTranslator() {
+		return getJdbcTemplate().getExceptionTranslator();
+	}
+
+	/**
+	 * Get a JDBC Connection, either from the current transaction or a new one.
+	 * @return the JDBC Connection
+	 * @throws CannotGetJdbcConnectionException if the attempt to get a Connection failed
+	 * @see org.springframework.jdbc.datasource.DataSourceUtils#getConnection(javax.sql.DataSource)
+	 */
+	protected final Connection getConnection() throws CannotGetJdbcConnectionException {
+		return DataSourceUtils.getConnection(this.getDataSource());
+	}
+
+	/**
+	 * Close the given JDBC Connection, created via this DAO's DataSource,
+	 * if it isn't bound to the thread.
+	 * @param con Connection to close
+	 * @see org.springframework.jdbc.datasource.DataSourceUtils#releaseConnection
+	 */
+	protected final void releaseConnection(Connection con) {
+		DataSourceUtils.releaseConnection(con, this.getDataSource());
+	}
+
+	public DataSource getDataSource() {
+		return dataSource;
+	}
+
 
 }
